a7838225be764e73e0a0967e27f3c6c48239117f
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-26 23:08:15 +0300

    Task 2: C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Route /c/<text>"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/c/is_fun", strict_slashes=False)
+def ctext():
+    """ C text """
+    return "C is fun"
+
+
+@app.route("/c/cool", strict_slashes=False)
+def ctext2():
+    """ Second C Text """
+    return "C cool"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
