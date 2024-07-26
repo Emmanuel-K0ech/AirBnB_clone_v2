009d173393c105450f5728aea80c6a841b0e94ab
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-26 16:43:58 +0300

    Task 0: Hello Flask!

---
+++ 0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This is a simple Flask application displaying text on screen """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Function Displaying the text on screen """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
